1d4392d081891dcb49960dca3116179936a80791
biorobobuilder <victorstepanov2019@u.northwestern.edu>
2025-04-01 10:33:30 -0400

    Impl nearst node search + tree init

--- src/RRTPlanner.cpp
+++ src/RRTPlanner.cpp
@@ -21,3 +21,46 @@ Point RRTPlanner::sample() {
     int y = std::rand() % rows_;
     return {x, y};
 }
+
+/**
+ * Computes Euclidean distance between two grid points.
+ */
+double RRTPlanner::distance(const Point& a, const Point& b) {
+    int dx = a.first - b.first;
+    int dy = a.second - b.second;
+    return std::sqrt(dx * dx + dy * dy);
+}
+
+/**
+ * Finds the closest node already in the tree.
+ */
+int RRTPlanner::getNearestNodeIndex(const Point& target) {
+    double minDist = std::numeric_limits<double>::max();
+    int nearestIdx = -1;
+
+    for (int i = 0; i < tree_.size(); ++i) {
+        double d = distance(tree_[i].pos, target);
+        if (d < minDist) {
+            minDist = d;
+            nearestIdx = i;
+        }
+    }
+
+    return nearestIdx;
+}
+
+/**
+ * Concept: in each RRT iteration:
+ *  1. Sample random point
+ *  2. Find the nearest node in the tree
+ *  3. Steer that node toward the sample
+ *  4. If it's collision-free, add it to the free
+ *
+ */
+std::vector<Point> RRTPlanner::plan(Point start, Point goal) {
+    tree_.clear();
+    tree_.push_back({start, -1});   // root node has no parent
+
+    // TODO: Loop and grow tree
+    return {};
+}
