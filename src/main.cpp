ced19534762bd021ebc243164f310d0a12de982a
biorobobuilder <victorstepanov2019@u.northwestern.edu>
2025-04-02 17:05:35 -0400

    Wire in docking mission

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,34 @@
 #include "planner/RRTPlanner.h"
 #include "GridVisualizer.h"
 #include "TrajectoryGenerator.h"
+#include "behavior/docking/DockingMission.h"
+#include <thread>
 
 constexpr int GRID_SIZE = 100;
 constexpr double ROBOT_RADIUS = 2.0;
 
 int main(int argc, char* argv[]) {
     if (argc != 6) {
-        std::cerr << "Usage: " << argv[0] << " <start_x> <start_y> <goal_x> <goal_y> <planner_flag>\n";
+        std::cerr << "Usage: " << argv[0] << " <start_x> <start_y> <goal_x> <goal_y> <planner_flag|behavior>\n";
         return 1;
     }
 
     std::pair<int, int> start = {std::stoi(argv[1]), std::stoi(argv[2])};
     std::pair<int, int> goal  = {std::stoi(argv[3]), std::stoi(argv[4])};
     std::string plannerFlag = argv[5];
+
+    if (plannerFlag == "behavior") {
+    std::unique_ptr<Behavior> mission = std::make_unique<DockingMission>();
+    mission->initialize();
+
+    while (!mission->isComplete()) {
+        mission->update();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    std::cout << "[Main] Mission '" << mission->name() << "' completed.\n";
+    return 0;
+}
 
     // Build Configuration Space
     CSpaceBuilder cspace(GRID_SIZE, ROBOT_RADIUS);
